69c6563b33aff834b0af9af974e8b4a53f3de11c
GTOVD <thomas.vickers@gtovd.com>
2026-02-26 23:10:04 -0800

    feat: complete Value Unit Ledger and settlement integration (Issue #48)

---
+++ dist/types/ledger.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * Zown Nexus Ledger Protocol (v0.1.0)
+ *
+ * Defines the transaction and balance structures for the agentic economy.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
